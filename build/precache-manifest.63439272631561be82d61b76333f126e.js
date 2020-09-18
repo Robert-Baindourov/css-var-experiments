d430cbf44f6b40555467dbf0db1e5818ee45b331
Robert Baindourov <robert@webdesigner.la>
2020-09-18 14:25:59 -0700

    saving css un-minimized

---
+++ build/precache-manifest.63439272631561be82d61b76333f126e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ae274c7a04c72fe2db255b4e01ed90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f1248ff88d270f49401",
+    "url": "/static/css/main.ca477ddd.chunk.css"
+  },
+  {
+    "revision": "b09809e8b2984a46f2a4",
+    "url": "/static/js/2.49f18f75.chunk.js"
+  },
+  {
+    "revision": "1f1248ff88d270f49401",
+    "url": "/static/js/main.2e89469c.chunk.js"
+  },
+  {
+    "revision": "f54ef3a4501161b45ccf",
+    "url": "/static/js/runtime-main.98ad43fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
