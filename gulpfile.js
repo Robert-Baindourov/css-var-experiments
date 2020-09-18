da3d7dacd1aa4ad77c12abb9f02c136d4298e19e
Robert Baindourov <robert@webdesigner.la>
2020-09-18 15:38:01 -0700

    saving

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
 const postcss = require ('gulp-postcss');
 const autoprefixer = require ('autoprefixer');
 const cssnano = require ('cssnano');
+const cssvariables = require("postcss-css-variables");
 const gulp = require ('gulp');
 
 gulp.task( 'css', ()=>{
     const plugin = [
-        autoprefixer({browsers: ['safari 7']}),        
+        autoprefixer({browsers: ['safari 7']}),
+        cssvariables()        
     ];
     return gulp.src('./build/static/css/*.css')
         .pipe(postcss(plugin))
         .pipe(gulp.dest('./dest'))
 })
-
-gulp.task( 'serve', gulp.series( 'css', ()=>{
-    gulp.watch("./build/static/css/*.css")
-}))
 
 gulp.task('default', gulp.series( 'css',( done )=>{
     done();
