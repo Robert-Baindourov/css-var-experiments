ea5abc49c4fb98b671fff3c3c930100654be50d9
Robert Baindourov <robert@webdesigner.la>
2020-09-18 10:16:08 -0700

    saving

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './_variables.scss';
-import './App.scss';
+import './scss/_variables.scss';
+import './scss/App.scss';
 
 function App() {
   return (
